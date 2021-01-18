3305eccdaac291be2afcced3778b29e5080b76e6
spelap1 <spelap1@users.noreply.github.com>
2021-01-18 23:14:43 +0100

    veva

---
+++ script.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("mysideNav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  
+}
+
+function closeNav() {
+  document.getElementById("mysideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
